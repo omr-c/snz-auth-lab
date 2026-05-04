392ecd9334724549bfb96261ea6fba11a506344b
omr-c <omar.carmona506@gmail.com.com>
2026-05-04 13:13:56 -0600

    feat: setup lab con java 21 esenezeta

---
+++ src/main/java/com/esenezeta/lab_security_jwt/LabSecurityJwtApplication.java
@@ -0,0 +1,13 @@
+package com.esenezeta.lab_security_jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabSecurityJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabSecurityJwtApplication.class, args);
+	}
+
+}
